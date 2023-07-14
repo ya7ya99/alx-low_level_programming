c927b8fece83013f67c21c9f91f7a0b948b1a9cf
ya7ya99 <yahyakhaldy9@gmail.com>
2023-07-14 08:51:54 -0700

    0x01 c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

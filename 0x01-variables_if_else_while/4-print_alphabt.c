f953a2cec76afd815f1297e0cd726983e1d4d148
ya7ya99 <yahyakhaldy9@gmail.com>
2023-07-14 09:47:47 -0700

    0x01 c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

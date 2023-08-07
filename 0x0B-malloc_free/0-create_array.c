f22f69e6c007f2333cc53fae06c1ee8f74b5a4e5
ya7ya99 <yahyakhaldy9@gmail.com>
2023-08-07 09:15:09 -0700

    0x0b

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,12 +7,13 @@
  *
  * Return: pointer to the array (Success), NULL (Error)
  */
-char *create_array(unsigned int size, char c){
+char *create_array(unsigned int size, char c)
+{
 	char *n = malloc(size);
 
 	if (size == 0 || n == 0)
-		return 0;
+		return (0);
 	while (size--)
 		n[size] = c;
-	return n;
+	return (n);
 }

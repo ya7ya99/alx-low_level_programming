00270203cd6a759663fc184988ba17c5abd2ad12
ya7ya99 <yahyakhaldy9@gmail.com>
2023-08-24 09:30:12 -0700

    printf test

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -21,3 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#endif
